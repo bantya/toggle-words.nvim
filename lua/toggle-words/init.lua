f1fef2a56a7ef86a7a07f59be89792f8d5c22609
Rahul Thakare <16685565+bantya@users.noreply.github.com>
2024-03-25 02:41:14 +0530

    First working draft

--- lua/toggle-words/init.lua
+++ lua/toggle-words/init.lua
@@ -3,33 +3,45 @@ local config = {
 }
 
 local function run()
-  -- save current cursor position
+  -- get current cursor position
   local save_cursor = vim.api.nvim_win_get_cursor(0)
 
   -- get word under cursor
   local current_word = vim.fn.expand("<cword>")
-  local new_word = vim.fn.input('replace ' .. current_word .. ' with: ')
+  local new_word = ''
+  local found_word = false
 
-  -- move cursor to the beginning of the word
-  vim.cmd('normal! b')
+  -- find the mapping for current word (word under cursor)
+  for key, values in pairs(config) do
+    local current_index = 2
 
-  -- delete the word under cursor
-  vim.cmd('normal! diwh')
+    for idx, value in ipairs(values) do
+      if value == current_word then
+        local index = (current_index + idx - 2) % #values + 1
+        new_word = values[index]
+        found_word = true
+      end
+    end
+  end
+
+  -- if no mapping is found, throw error
+  if found_word == false then
+    vim.api.nvim_err_writeln("Replacement not provided!")
+    return
+  end
+
+  -- move cursor to the beginning of the word and delete the word under cursor
+  vim.cmd('normal! bdiw')
 
   -- insert the new word
   vim.api.nvim_put({new_word}, '', true, true)
 
   -- restore cursor position
-  vim.api.aa(0, save_cursor)
+  vim.api.nvim_win_set_cursor(0, save_cursor)
 end
 
 local function setup(user_config)
-  print(user_config)
-  for key, value in pairs(user_config) do
-    if config[key] ~= nil then
-      config[key] = value
-    end
-  end
+  config = vim.tbl_extend("force", config, user_config or {})
 end
 
 return {
